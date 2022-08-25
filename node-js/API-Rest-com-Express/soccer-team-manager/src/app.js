ca224800047b5a182cf11bf6b31791790026863c
will <gomeswillderson@gmail.com>
2022-08-25 20:17:49 -0300

    feat: PUSH editando dados atravez do metodo PUSH

--- node-js/API-Rest-com-Express/soccer-team-manager/src/app.js
+++ node-js/API-Rest-com-Express/soccer-team-manager/src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
-
 const teams = [
     {
       id: 1,
@@ -15,7 +13,25 @@ const teams = [
       name: 'Clube Atlético Mineiro',
       initials: 'CAM',
     },
-  ];
+];
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
@@ -24,8 +40,12 @@ app.get('/teams', (req, res) => res.status(200).json({ teams }));
 app.post('/teams', (req, res) => {
     const newTeam = { ...req.body };
     teams.push(newTeam);
-  
     res.status(201).json({ team: newTeam });
+});
+
+app.get('/teams/:id', (req, res) => {
+    const team = teams.find(({ id }) => id === Number(req.params.id));
+    res.status(200).json(team);
   });
 
 module.exports = app;
